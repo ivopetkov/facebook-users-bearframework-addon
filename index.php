f99907aa1fa9b24bd6b98c628d76a0fc3c714f23
Ivo Petkov <ivo@ivopetkov.com>
2017-03-06 15:58:38 +0200

    Formatting update.

--- index.php
+++ index.php
@@ -33,50 +33,50 @@ $getAddonOptions = function() use ($app) {
     return $addonOptions;
 };
 
-$app->routes->add('/-ivopetkov-facebook-user-redirect', function() use ($app, $getAddonOptions) {
-    $addonOptions = $getAddonOptions();
-    $referer = (string)$app->request->query->getValue('referer');
-    $redirectUri = $app->urls->get('/-ivopetkov-facebook-user-auth?referer=' . rawurlencode($referer));
-    $url = 'https://www.facebook.com/v2.8/dialog/oauth?client_id=' . $addonOptions['facebookAppID'] . '&redirect_uri=' . urlencode($redirectUri);
-    $response = new App\Response\TemporaryRedirect($url);
-    return $response;
-});
+$app->routes
+        ->add('/-ivopetkov-facebook-user-redirect', function() use ($app, $getAddonOptions) {
+            $addonOptions = $getAddonOptions();
+            $referer = (string) $app->request->query->getValue('referer');
+            $redirectUri = $app->urls->get('/-ivopetkov-facebook-user-auth?referer=' . rawurlencode($referer));
+            $url = 'https://www.facebook.com/v2.8/dialog/oauth?client_id=' . $addonOptions['facebookAppID'] . '&redirect_uri=' . urlencode($redirectUri);
+            $response = new App\Response\TemporaryRedirect($url);
+            return $response;
+        })
+        ->add('/-ivopetkov-facebook-user-auth', function() use ($app, $getAddonOptions) {
+            $referer = (string) $app->request->query->getValue('referer');
+            $code = (string) $app->request->query->getValue('code');
 
-$app->routes->add('/-ivopetkov-facebook-user-auth', function() use ($app, $getAddonOptions) {
-    $referer = (string)$app->request->query->getValue('referer');
-    $code = (string)$app->request->query->getValue('code');
+            $addonOptions = $getAddonOptions();
 
-    $addonOptions = $getAddonOptions();
+            $makeRequest = function($url) {
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                    CURLOPT_RETURNTRANSFER => 1,
+                    CURLOPT_URL => $url,
+                    CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+                ));
+                $response = curl_exec($curl);
+                curl_close($curl);
+                return $response;
+            };
 
-    $makeRequest = function($url) {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => $url,
-            CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return $response;
-    };
+            $redirectUri = $app->urls->get('/-ivopetkov-facebook-user-auth?referer=' . rawurlencode($referer));
 
-    $redirectUri = $app->urls->get('/-ivopetkov-facebook-user-auth?referer=' . rawurlencode($referer));
-
-    $tokenUrl = 'https://graph.facebook.com/v2.8/oauth/access_token?client_id=' . $addonOptions['facebookAppID'] . '&redirect_uri=' . urlencode($redirectUri) . '&client_secret=' . $addonOptions['facebookAppSecret'] . '&code=' . rawurlencode($code);
-    $response = $makeRequest($tokenUrl);
-    $parts = json_decode($response, true);
-    if (isset($parts['access_token'])) {
-        $accessToken = $parts['access_token'];
-        $userData = json_decode($makeRequest("https://graph.facebook.com/v2.8/me?access_token=" . urlencode($accessToken)), true);
-        if (isset($userData['id'], $userData['name'])) {
-            $id = md5('facebook' . $userData['id']);
-            $app->users->saveUserData('facebook', $id, [
-                'id' => $userData['id'],
-                'name' => $userData['name']
-            ]);
-            $app->currentUser->set('facebook', $id);
-        }
-    }
-    $response = new App\Response\TemporaryRedirect($referer);
-    return $response;
-});
+            $tokenUrl = 'https://graph.facebook.com/v2.8/oauth/access_token?client_id=' . $addonOptions['facebookAppID'] . '&redirect_uri=' . urlencode($redirectUri) . '&client_secret=' . $addonOptions['facebookAppSecret'] . '&code=' . rawurlencode($code);
+            $response = $makeRequest($tokenUrl);
+            $parts = json_decode($response, true);
+            if (isset($parts['access_token'])) {
+                $accessToken = $parts['access_token'];
+                $userData = json_decode($makeRequest("https://graph.facebook.com/v2.8/me?access_token=" . urlencode($accessToken)), true);
+                if (isset($userData['id'], $userData['name'])) {
+                    $id = md5('facebook' . $userData['id']);
+                    $app->users->saveUserData('facebook', $id, [
+                        'id' => $userData['id'],
+                        'name' => $userData['name']
+                    ]);
+                    $app->currentUser->set('facebook', $id);
+                }
+            }
+            $response = new App\Response\TemporaryRedirect($referer);
+            return $response;
+        });
