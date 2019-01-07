397765f1ede526db2d24cf6caaa05f52e9af451b
Ivo Petkov <ivo@ivopetkov.com>
2019-01-07 11:57:58 +0200

    Added FacebookLoginProvider::handleOAuthRedirect().

--- classes/FacebookLoginProvider.php
+++ classes/FacebookLoginProvider.php
@@ -55,4 +55,65 @@ class FacebookLoginProvider implements ILoginProvider
         return $properties;
     }
 
+    /**
+     * 
+     * @return \BearFramework\App\Response|null
+     */
+    static function handleOAuthRedirect()
+    {
+        $app = App::get();
+        $referer = base64_decode((string) $app->request->query->getValue('state'));
+        if (!is_string($referer)) {
+            return null;
+        }
+        $code = (string) $app->request->query->getValue('code');
+
+        $getAddonOptions = function() use ($app) {
+            $addonOptions = $app->addons->get('ivopetkov/facebook-users-bearframework-addon')->options;
+            if (!isset($addonOptions['facebookAppID'])) {
+                throw new \Exception('The ivopetkov/facebook-users-bearframework-addon facebookAppID option is required');
+            }
+            if (!isset($addonOptions['facebookAppSecret'])) {
+                throw new \Exception('The ivopetkov/facebook-users-bearframework-addon facebookAppSecret option is required');
+            }
+            if (!isset($addonOptions['oauthRedirectUrl'])) {
+                throw new \Exception('The ivopetkov/facebook-users-bearframework-addon oauthRedirectUrl option is required');
+            }
+            return $addonOptions;
+        };
+
+        $addonOptions = $getAddonOptions();
+
+        $makeRequest = function($url) {
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_URL => $url,
+                CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            return $response;
+        };
+
+        $tokenUrl = 'https://graph.facebook.com/v3.2/oauth/access_token?client_id=' . $addonOptions['facebookAppID'] . '&redirect_uri=' . urlencode($addonOptions['oauthRedirectUrl']) . '&client_secret=' . $addonOptions['facebookAppSecret'] . '&code=' . rawurlencode($code);
+        $response = $makeRequest($tokenUrl);
+        $parts = json_decode($response, true);
+        if (isset($parts['access_token'])) {
+            $accessToken = $parts['access_token'];
+            $userData = json_decode($makeRequest("https://graph.facebook.com/v3.2/me?access_token=" . urlencode($accessToken)), true);
+            if (isset($userData['id'], $userData['name'])) {
+                $id = $userData['id'];
+                $app->users->saveUserData('facebook', $id, [
+                    'id' => $id,
+                    'name' => $userData['name']
+                ]);
+                $app->currentUser->login('facebook', $id);
+            }
+        }
+        $response = new App\Response\TemporaryRedirect($referer);
+        $response->headers->set($response->headers->make('Cache-Control', 'no-cache, no-store, must-revalidate'));
+        return $response;
+    }
+
 }
--- index.php
+++ index.php
@@ -48,44 +48,7 @@ $app->routes
         ->add('*', function() use ($app, $getAddonOptions) {
             $addonOptions = $getAddonOptions();
             if ($app->request->base . $app->request->path === $addonOptions['oauthRedirectUrl']) {
-                $referer = base64_decode((string) $app->request->query->getValue('state'));
-                if (!is_string($referer)) {
-                    return;
-                }
-                $code = (string) $app->request->query->getValue('code');
-
-                $addonOptions = $getAddonOptions();
-
-                $makeRequest = function($url) {
-                    $curl = curl_init();
-                    curl_setopt_array($curl, array(
-                        CURLOPT_RETURNTRANSFER => 1,
-                        CURLOPT_URL => $url,
-                        CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
-                    ));
-                    $response = curl_exec($curl);
-                    curl_close($curl);
-                    return $response;
-                };
-
-                $tokenUrl = 'https://graph.facebook.com/v3.2/oauth/access_token?client_id=' . $addonOptions['facebookAppID'] . '&redirect_uri=' . urlencode($addonOptions['oauthRedirectUrl']) . '&client_secret=' . $addonOptions['facebookAppSecret'] . '&code=' . rawurlencode($code);
-                $response = $makeRequest($tokenUrl);
-                $parts = json_decode($response, true);
-                if (isset($parts['access_token'])) {
-                    $accessToken = $parts['access_token'];
-                    $userData = json_decode($makeRequest("https://graph.facebook.com/v3.2/me?access_token=" . urlencode($accessToken)), true);
-                    if (isset($userData['id'], $userData['name'])) {
-                        $id = $userData['id'];
-                        $app->users->saveUserData('facebook', $id, [
-                            'id' => $id,
-                            'name' => $userData['name']
-                        ]);
-                        $app->currentUser->login('facebook', $id);
-                    }
-                }
-                $response = new App\Response\TemporaryRedirect($referer);
-                $response->headers->set($response->headers->make('Cache-Control', 'no-cache, no-store, must-revalidate'));
-                return $response;
+                return FacebookLoginProvider::handleOAuthRedirect();
             }
         })
         ->add('/-ivopetkov-facebook-user-redirect', function() use ($app, $getAddonOptions) {
