b12d335887193d73c855a2b17db1a0fdc86005d1
Ivo Petkov <ivo@ivopetkov.com>
2021-01-17 23:21:47 +0200

    Code format

--- index.php
+++ index.php
@@ -14,19 +14,18 @@ $app = App::get();
 $context = $app->contexts->get(__DIR__);
 
 $context->classes
-        ->add(FacebookUserProvider::class, 'classes/FacebookUserProvider.php');
+    ->add(FacebookUserProvider::class, 'classes/FacebookUserProvider.php');
 
 $app->users
-        ->addProvider('facebook', FacebookUserProvider::class);
+    ->addProvider('facebook', FacebookUserProvider::class);
 
 $app->localization
-        ->addDictionary('en', function() use ($context) {
-            return include $context->dir . '/locales/en.php';
-        })
-        ->addDictionary('bg', function() use ($context) {
-            return include $context->dir . '/locales/bg.php';
-        })
-        ->addDictionary('ru', function() use ($context) {
-            return include $context->dir . '/locales/ru.php';
-        });
-
+    ->addDictionary('en', function () use ($context) {
+        return include $context->dir . '/locales/en.php';
+    })
+    ->addDictionary('bg', function () use ($context) {
+        return include $context->dir . '/locales/bg.php';
+    })
+    ->addDictionary('ru', function () use ($context) {
+        return include $context->dir . '/locales/ru.php';
+    });
